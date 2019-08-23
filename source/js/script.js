47886ecbdb129aee8b8732d7a2c47f95113d76e6
edji777 <49676371+edji777@users.noreply.github.com>
2019-08-23 12:13:45 +0300

    внес исправления

--- source/js/script.js
+++ source/js/script.js
@@ -1,24 +1,23 @@
-var menu = document.querySelector(".main-nav__list");
-var nav =  document.querySelector(".main-nav");
+var menu = document.querySelector(".site-list");
 var logo =  document.querySelector(".page-header__logo");
 var toggle = document.querySelector(".main-nav__toggle");
 
-nav.classList.remove('show');
-nav.classList.add('hide');
+menu.classList.remove("show");
+menu.classList.add("hide");
 
 
 toggle.addEventListener("click", function (evt) {
   evt.preventDefault();
-  if (nav.classList.contains("show")) {
-    nav.classList.remove("show");
-    nav.classList.add("hide");
+  if (menu.classList.contains("show")) {
+    menu.classList.remove("show");
+    menu.classList.add("hide");
     logo.classList.add("top");
     toggle.classList.remove("main-nav__toggle--cross");
     toggle.classList.add("main-nav__toggle--burger");
   }
   else {
-    nav.classList.remove("hide");
-    nav.classList.add("show");
+    menu.classList.remove("hide");
+    menu.classList.add("show");
     logo.classList.remove("top");
     toggle.classList.remove("main-nav__toggle--burger");
     toggle.classList.add("main-nav__toggle--cross");
